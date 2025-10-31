e3eb81d0718031b77299729b7b94af61324fb057
Cade <cademedearis@gmail.com>
2025-10-31 16:59:14 -0500

    Added terminal output to show availability, now working on opening a MIDI device and getting output

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -4,6 +4,19 @@
 MainComponent::MainComponent()
 {
     setSize (600, 400);
+    //Seeing what MIDI devices are available
+    auto midiInputList = juce::MidiInput::getAvailableDevices();
+    if (midiInputList.isEmpty()) {
+        juce::Logger::writeToLog("No MIDI input devices found.");
+    } 
+    else {
+        for (int i = 0; i < midiInputList.size(); i++) {
+            //g.drawText(midiInputs[i].name + " is available.", getLocalBounds(), juce::Justification::centred, true);
+            juce::Logger::writeToLog("MIDI Input Device " + juce::String(i) + ": " + midiInputList[i].name + " ID: " + midiInputList[i].identifier + ")");
+        }
+	    //MY SPECIFIC DEVICE, CHANGE TO BE DYNAMIC LATER
+        midiInputDevice = juce::MidiInput::openDevice(midiInputList[4].identifier, this);
+    }
 }
 
 MainComponent::~MainComponent()
@@ -18,7 +31,9 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::FontOptions (16.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+
+
+    //g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
@@ -26,4 +41,19 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+}
+
+
+void MainComponent::handleIncomingMidiMessage(juce::MidiInput* source, const juce::MidiMessage& message) {
+	// Handle incoming MIDI messages here
+    int noteNumber = message.getNoteNumber();
+    juce::String noteName = message.getMidiNoteName(noteNumber, true, true, 4);
+    juce::uint8 noteVelocity = message.getVelocity();
+    if (message.isNoteOn()) {
+        juce::Logger::writeToLog(noteName + "pressed with velocity " + juce::String(noteVelocity));
+    }
+    else if (message.isNoteOff()) {
+		juce::Logger::writeToLog(noteName + " released");
+    }
+    
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include <memory>
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::Component, public juce::MidiInputCallback
 {
 public:
     //==============================================================================
@@ -18,10 +19,12 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+	void handleIncomingMidiMessage(juce::MidiInput* source, const juce::MidiMessage& message) override; //The callback for incoming MIDI messages
+
 private:
     //==============================================================================
     // Your private member variables go here...
-
+	std::unique_ptr<juce::MidiInput> midiInputDevice; //Pointer to the MIDI input device
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
