bfe1b2e25523eb18fd59e4233be0a0c266b0860f
Cade <cademedearis@gmail.com>
2025-10-31 18:21:39 -0500

    Created empty PianoKeyboard class, need to fill out with functions and variables

---
+++ Source/PianoKeyboard.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    PianoKeyboard.h
+    Created: 31 Oct 2025 6:18:38pm
+    Author:  Cade Medearis
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h> 
+
+class PianoKeyboard : public juce::Component
+{
+public:
+
+
+private:
+};
